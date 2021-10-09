16125f57884499ba01213e64d21e1df337970932
JDorsey22 <jonathandorsey1117@gmail.com>
2021-10-09 12:39:59 -0700

    [Feature] Visualize Images and Masks for testing Datasets/Dataloader process

--- Carvana_Dataset/CarvanaDS.py
+++ Carvana_Dataset/CarvanaDS.py
@@ -62,8 +62,8 @@ class CarvanaData(Dataset):
         seed_left = np.random.randint(0, 1408)
 
         # Apply random resize/crop transform to both image/mask
-        image = tf.resized_crop(image, seed_top, seed_left, 512, 512, (self.image_height, self.image_width))
-        mask = tf.resized_crop(mask, seed_top, seed_left, 512, 512, (self.image_height, self.image_width))
+        image = tf.resized_crop(image, seed_top, seed_left, 512, 512, [self.image_height, self.image_width])
+        mask = tf.resized_crop(mask, seed_top, seed_left, 512, 512, [self.image_height, self.image_width])
 
         # Convert to tensor
         image = tf.to_tensor(image)
@@ -71,7 +71,7 @@ class CarvanaData(Dataset):
 
         # Optional Image Normalization
         if normalize:
-            normalized_img = tf.normalize(image, (0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            normalized_img = tf.normalize(image, [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             return normalized_img, mask
         return image, mask
 
@@ -116,3 +116,57 @@ class CarvanaData(Dataset):
             image, mask = Image.fromarray(image), Image.fromarray(mask)
             image, mask = self.resize_crop_transform(image, mask)
             return image, mask
+
+
+def show_batched_images(num_images=10):
+    # from Carvana_Dataset.CarvanaDS import CarvanaData
+    from torch.utils.data import DataLoader
+    import torch
+    import matplotlib.pyplot as plt
+
+    batch_size = 2
+    train_ds = CarvanaData()
+    DL = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=4)
+
+    for ind, batch in enumerate(DL):
+        image, mask = batch
+
+        real_image = image[0][:][:][:]
+
+        real_image = real_image.permute(1, 2, 0)
+        plt.imshow(real_image)
+        plt.show()
+
+        if ind >= num_images:
+            break
+
+
+def show_batched_masks(num_masks=10):
+    # from Carvana_Dataset.CarvanaDS import CarvanaData
+    from torch.utils.data import DataLoader
+    import torch
+    import matplotlib.pyplot as plt
+
+    batch_size = 2
+    train_ds = CarvanaData()
+    DL = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=4)
+
+    for ind, batch in enumerate(DL):
+        image, mask = batch
+        real_mask = mask[0][:][:][:]
+
+        real_mask = real_mask.permute(1, 2, 0)
+        plt.imshow(real_mask)
+        plt.show()
+
+        if ind >= num_masks:
+            break
+
+
+if __name__ == '__main__':
+
+    # show_batched_images()
+    show_batched_masks()
+
+
+
--- Carvana_Dataset/ValDS.py
+++ Carvana_Dataset/ValDS.py
@@ -75,3 +75,53 @@ class ValidationData(Dataset):
         image, mask = Image.fromarray(image), Image.fromarray(mask)
         image, mask = self.resize_transform(image, mask)
         return image, mask
+
+def show_batched_images(num_images=10):
+    # from Carvana_Dataset.CarvanaDS import CarvanaData
+    from torch.utils.data import DataLoader
+    import torch
+    import matplotlib.pyplot as plt
+
+    batch_size = 2
+    train_ds = ValidationData()
+    DL = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=4)
+
+    for ind, batch in enumerate(DL):
+        image, mask = batch
+
+        real_image = image[0][:][:][:]
+
+        real_image = real_image.permute(1, 2, 0)
+        plt.imshow(real_image)
+        plt.show()
+
+        if ind >= num_images:
+            break
+
+
+def show_batched_masks(num_masks=10):
+    # from Carvana_Dataset.CarvanaDS import CarvanaData
+    from torch.utils.data import DataLoader
+    import torch
+    import matplotlib.pyplot as plt
+
+    batch_size = 2
+    train_ds = ValidationData()
+    DL = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=4)
+
+    for ind, batch in enumerate(DL):
+        image, mask = batch
+        real_mask = mask[0][:][:][:]
+
+        real_mask = real_mask.permute(1, 2, 0)
+        plt.imshow(real_mask)
+        plt.show()
+
+        if ind >= num_masks:
+            break
+
+
+if __name__ == '__main__':
+
+    # show_batched_images()
+    show_batched_masks()
