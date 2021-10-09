7f5feea30f085610cc28d6d60059bf287b4869c4
JDorsey22 <jonathandorsey1117@gmail.com>
2021-10-08 23:52:13 -0700

    [Refactor] Refactor of CarvanaDS for clarity

--- RGB_Segmentation/CarvanaDS.py
+++ RGB_Segmentation/CarvanaDS.py
@@ -2,137 +2,117 @@ import os
 import cv2
 import numpy as np
 from PIL import Image
-from matplotlib import pyplot as plt
-import logging
-import sys
-
-import torch
-import torchvision
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import Dataset, DataLoader, random_split
-from torch.autograd import Function
-
 from torchvision import transforms, datasets
-import torchvision.transforms.functional as TF
-
+import torchvision.transforms.functional as tf
 from albumentations import (Compose, RandomCrop, Resize, HorizontalFlip, ShiftScaleRotate, RandomResizedCrop, RandomBrightnessContrast, ElasticTransform, IAAAffine, IAAPerspective, OneOf)
 
 
-
-
-
-
-
 class CarvanaData(Dataset):
-    def __init__(self, root_dir= r"C:\Users\Indy-Windows\Desktop\carvana\carvana\data\train", transform=None, image_size=(512, 512)):
+    def __init__(self, root_dir=r"C:\Users\Indy-Windows\Desktop\carvana\carvana\data\train", transform=None, image_size=(512, 512)):
 
-        #Initialize Directory Tree from current working directory if no directory is provided
+        # Initialize Directory Tree from current working directory if no directory is provided
         self.root_dir = root_dir
+
+        # Get Image/Mask Path
         self.img_dir = os.path.join(self.root_dir, 'images')
         self.mask_dir = os.path.join(self.root_dir, 'masks')
 
-        self.img_transform = transform
-        self.num_img = len(os.listdir(self.img_dir))
-        self.num_mask = len(os.listdir(self.mask_dir))
-
+        # Get List of Images/Masks in Carvana Directory
         self.img_list = os.listdir(self.img_dir)
         self.mask_list = os.listdir(self.mask_dir)
 
+        # Get Number of Images/Masks
+        self.num_img = len(self.img_list)
+        self.num_mask = len(self.mask_list)
+
+        # Define Transform Image Dimensions
         self.image_height = image_size[1]
         self.image_width = image_size[0]
 
+        # Define Custom Augmentation Transform
         self.transform = transform
 
+        # Define Default Aug Transformation
         self.album_transform = Compose([
             HorizontalFlip(),
-            ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.2),
+            ShiftScaleRotate(
+                shift_limit=0.0625,
+                scale_limit=0.2,
+                rotate_limit=45,
+                p=0.2),
             OneOf([
                 ElasticTransform(p=.2),
                 IAAPerspective(p=.35),
             ], p=.35)
         ])
 
-        if self.transform == None:
-            self.transform = self.album_transform
-
-
-
-    def segment_transform(self, image, mask):
-
+    def resize_crop_transform(self, image, mask, normalize=False):
+        """
+        Applies Random Crop/Resize Transform to Image/Mask Tuple.
+        This operation requires that both the mask and the image
+        are transformed identically.
+        :param normalize: (applies normalization if True)
+        :param image:
+        :param mask:
+        :return: cropped & resized image/mask Pytorch tensors
+        """
+        # Define Random Seeds for Image/Mask Resize/Crop transform
         seed_top = np.random.randint(0, 568)
         seed_left = np.random.randint(0, 1408)
 
-        image = TF.resized_crop(image, seed_top, seed_left, 512, 512, (self.image_height, self.image_width))
+        # Apply random resize/crop transform to both image/mask
+        image = tf.resized_crop(image, seed_top, seed_left, 512, 512, (self.image_height, self.image_width))
+        mask = tf.resized_crop(mask, seed_top, seed_left, 512, 512, (self.image_height, self.image_width))
 
-        mask = TF.resized_crop(mask, seed_top, seed_left, 512, 512, (self.image_height, self.image_width))
-        #cropped = transforms.RandomCrop(size=(self.image_height, self.image_width))
+        # Convert to tensor
+        image = tf.to_tensor(image)
+        mask = tf.to_tensor(mask)
 
-        #
-        #
-        # # # Random crop
-        # i, j, h, w = transforms.RandomCrop.get_params(image, output_size=(int(1920*self.scale), int(1080*self.scale)))
-        # image = TF.crop(image, i, j, h, w)
-        # mask = TF.crop(mask, i, j, h, w)
-        #
-        # # Random horizontal flipping
-        # if random.random() > 0.5:
-        #     image = TF.hflip(image)
-        #     mask = TF.hflip(mask)
-
-        # if random.random() > 0.5:
-        #     angle = random.randint(-30, 30)
-        #     image = TF.rotate(image, angle)
-        #     mask = TF.rotate(mask, angle)
-
-
-        # print(f" Pre Image Max {np.max(image)} & Pre Image Min {np.min(image)}")
-        # print(f"PRE -Image Shape: ", image)
-
-
-        # Transform to tensor
-        image = TF.to_tensor(image)
-        mask = TF.to_tensor(mask)
-
-        # image = image / 255.0
-        # print(f"Post Image Max {torch.max(image)} & Post Image Min {torch.min(image)}")
-        # print(f"Image Shape: ", image)
-
-        # TF.normalize(image, (0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        # Optional Image Normalization
+        if normalize:
+            normalized_img = tf.normalize(image, (0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            return normalized_img, mask
         return image, mask
 
-
     def __len__(self):
-
+        """
+        Define the length of the dataset.
+        """
+        # Check if number of image/masks are equal
         if self.num_img == self.num_mask:
             return self.num_img
         else:
             raise Exception("Number of Images & GT Masks is NOT equal")
 
     def __getitem__(self, item):
-
+        """
+        Get the image/mask at index "item"
+        :return:
+        """
+        # Define full image/mask path for extracting data
         img_path = os.path.join(self.img_dir, self.img_list[item])
         mask_path = os.path.join(self.mask_dir, self.mask_list[item])
 
         # img = Image.open(img_path)
         # mask = Image.open(mask_path).convert('L')
 
+        # Use OpenCV to read Image/Masks from given paths
         image = cv2.imread(img_path)
         mask = cv2.imread(mask_path, 0)
 
-        if self.transform:
-            # augment = self.transform(image=image, mask=mask)
-            # image, mask = augment['image'], augment['mask']
-            #
+        # Apply Custom Transform
+        if self.transform is not None:
+            augment = self.transform(image=image, mask=mask)
+            image, mask = augment['image'], augment['mask']
+
             image, mask = Image.fromarray(image), Image.fromarray(mask)
-            image, mask = self.segment_transform(image, mask)
-
-
+            image, mask = self.resize_crop_transform(image, mask)
+            return image, mask
+        # Apply Default Transform
         else:
+            augment = self.album_transform(image=image, mask=mask)
+            image, mask = augment['image'], augment['mask']
             image, mask = Image.fromarray(image), Image.fromarray(mask)
-            image, mask = self.segment_transform(image, mask)
-
-
-        return image, mask
+            image, mask = self.resize_crop_transform(image, mask)
+            return image, mask
