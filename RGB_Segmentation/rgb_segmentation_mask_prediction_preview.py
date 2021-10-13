5da64bc651ef1519cdac3205b94b5780278b9fae
JDorsey22 <jonathandorsey1117@gmail.com>
2021-10-12 22:16:03 -0700

    [Feature] Added Comprehensive preview of image/ GT_Mask/ predicted mask from trained model and predicted mask from untrained model along with trained and untrained losses

--- RGB_Segmentation/rgb_segmentation_mask_prediction_preview.py
+++ RGB_Segmentation/rgb_segmentation_mask_prediction_preview.py
@@ -17,66 +17,113 @@ import torch
 import torchvision
 from torch import optim
 from torch.utils.data import DataLoader, random_split
-
-import pytorch_lightning as pl
-from pytorch_lightning.core import LightningModule
-from pytorch_lightning import Trainer
-from pytorch_lightning.profiler import SimpleProfiler, AdvancedProfiler
-
-from pytorch_lightning.loggers import TensorBoardLogger
-from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
-from pytorch_lightning.callbacks.model_checkpoint import Callback
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+import matplotlib.pyplot as plt
 
 from torchmetrics.functional import dice_score
 from pl_bolts.losses.object_detection import iou_loss
 
 # from RGB_Segmentation.unet import UNet
-from RGB_Segmentation.off_the_shelf_unet import UNet
+# from RGB_Segmentation.off_the_shelf_unet import UNet
+from RGB_Segmentation.off_the_shelf_unet_w_sigmoid import UNet
+from RGB_Segmentation.bce_dice_loss import DiceBCELoss
+
+
+def plot_img_mask_pred(image, mask, untrained_pred, trained_pred, no_train_loss, train_loss):
+    # Transform Tensors to Images
+    untrained_pred_mask = untrained_pred[0][0][:][:]
+    untrained_pred_mask = untrained_pred_mask.detach().numpy()
+
+    trained_pred_mask = trained_pred[0][0][:][:]
+    trained_pred_mask = trained_pred_mask.detach().numpy()
+
+    real_image = image[0][:][:][:]
+    real_image = real_image.permute(1, 2, 0)
+
+    real_mask = mask[0][:][:][:]
+    real_mask = real_mask.permute(1, 2, 0)
+
+    # create figure
+    fig = plt.figure(figsize=(15, 5))
+    fig.suptitle(f"Untrained Loss = {round(float(no_train_loss.detach().numpy()), 2)} & Trained Loss {round(float(trained_loss.detach().numpy()), 2)}")
+
+    # setting values to rows and column variables
+    rows = 1
+    columns = 4
+
+    # Adds a subplot at the 1st position
+    fig.add_subplot(rows, columns, 1)
+
+    # showing image
+    plt.imshow(real_image)
+    plt.axis('off')
+    plt.title("Image")
+
+    # Adds a subplot at the 2nd position
+    fig.add_subplot(rows, columns, 2)
+
+    # showing image
+    plt.imshow(real_mask)
+    plt.axis('off')
+    plt.title("Mask")
+
+    # Adds a subplot at the 3rd position
+    fig.add_subplot(rows, columns, 3)
+
+    # showing image
+    plt.imshow(untrained_pred_mask)
+    plt.axis('off')
+    plt.xlabel(f"UT Loss = {no_train_loss}")
+    plt.title("UT Mask")
+
+    # Adds a subplot at the 3rd position
+    fig.add_subplot(rows, columns, 4)
+
+    # showing image
+    plt.imshow(trained_pred_mask)
+    plt.axis('off')
+    plt.xlabel(f"T Loss = {train_loss}")
+    plt.title("T Mask")
+
+    print(f"T Loss = {train_loss} & UT Loss = {no_train_loss}")
+    plt.show()
+
+
+NUM_SAMPLES = 10
+VERSION = 15
+EPOCH = 44
+STEP = 10664
 
 model = UNet()
-# print(model.state_dict())
-checkpoint = torch.load(r"C:\Users\Indy-Windows\Documents\RGB-D-Plant-Segmentation\RGB_Segmentation\logs\my_model\version_15\checkpoints\epoch=33-step=8057.ckpt")
-# print(checkpoint['state_dict'])
-model.load_state_dict(checkpoint['state_dict'], strict=False)
-# print(model.state_dict())
+train_model = UNet()
+rootdir = r"C:\Users\Indy-Windows\Documents\RGB-D-Plant-Segmentation\RGB_Segmentation\logs\my_model"
+path = rootdir + f"\\version_{VERSION}\\checkpoints\\epoch={EPOCH}-step={STEP}.ckpt"
+checkpoint = torch.load(path)
+train_model.load_state_dict(checkpoint['state_dict'], strict=False)
 
 validation_dataloader = DataLoader(ValidationData(), shuffle=True, drop_last=True, batch_size=1)
+loss_criterion = DiceBCELoss()
 
 
-EPOCHS = 1
+for ind, batch in enumerate(validation_dataloader):
+    image, mask = batch
 
-for epoch in range(EPOCHS):
+    if ind < NUM_SAMPLES:
+        # Untrained Model Loss
+        y_hat_untrained = model(image)
+        untrained_loss = loss_criterion(y_hat_untrained, mask)
 
-    for batch in validation_dataloader:
-        image, mask = batch
+        # Trained Model
+        y_hat_trained = train_model(image)
+        trained_loss = loss_criterion(y_hat_trained, mask)
 
-        y_hat = model(image)
-        import matplotlib.pyplot as plt
-
-        # pred_mask = torch.squeeze(y_hat)
-        pred_mask = y_hat[0][0][:][:]
-        pred_mask = torch.sigmoid_(pred_mask) > .55
-        # print(f"Pred Mask Shape Post Squeeze = {pred_mask.shape}")
-
-        pred_mask = pred_mask.detach().numpy()
-
-        # print(pred_mask)
-        plt.imshow(pred_mask)
-        plt.show()
+        plot_img_mask_pred(image, mask, y_hat_untrained, y_hat_trained, untrained_loss, trained_loss)
+    break
 
 
-        # print(type(image))
-        #
-        # import matplotlib.pyplot as plt
-        # real_image = image[0][:][:][:]
-        # real_image = real_image.permute(1, 2, 0)
-        # plt.imshow(real_image)
-        # plt.show()
-        #
-        # real_mask = mask[0][:][:][:]
-        # real_mask = real_mask.permute(1, 2, 0)
-        # # plt.imshow(real_mask)
-        # # plt.show()
+# Train Model Loss
 
-        break
+
+
+
+
+
