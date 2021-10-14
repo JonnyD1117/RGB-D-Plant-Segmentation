2613b781ca5c428196a94a7807c577a787f4a52b
JDorsey22 <jonathandorsey1117@gmail.com>
2021-10-13 19:40:35 -0700

    [Feature] Add manual pytorch trainer script INSTEAD of pytorch lighnting

---
+++ RGB_Segmentation/trainer.py
@@ -0,0 +1,85 @@
+import os
+from argparse import ArgumentParser
+from tqdm import tqdm
+
+from Carvana_Dataset.CarvanaDS import CarvanaData
+from Carvana_Dataset.ValDS import ValidationData
+from Carvana_Dataset.Test_DS import TestData
+
+import torch
+import torchvision
+from torch import optim
+from torch.utils.data import DataLoader, random_split
+
+from RGB_Segmentation.off_the_shelf_unet import UNet
+from RGB_Segmentation.bce_dice_loss import DiceBCELoss
+
+
+if __name__ == '__main__':
+    # Define Training Parameters
+    EPOCHS = 10
+    lr = .003
+    batch_size = 2
+    val_batch_size = 1
+    img_height = 517
+    img_width = 517
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Define Model
+    model = UNet().to(device)
+
+    # Define Optimizer
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    # Define Loss Function
+    loss_criterion = DiceBCELoss()
+
+    # Create DataLoader
+    carvana_dl = DataLoader(TestData(), batch_size, shuffle=True, num_workers=4, drop_last=True)
+    validation_dl = DataLoader(ValidationData(), val_batch_size, shuffle=False, num_workers=4, drop_last=True)
+
+    # Loop Through All Epochs
+    for epoch in tqdm(range(EPOCHS)):
+        # Loop Through Each Batch
+        for image, mask in tqdm(carvana_dl):
+            print(f"Image Size = {image.shape}")
+            # Send Training Data to GPU
+            image = image.to(device)
+            mask = mask.to(device)
+
+            # UNET Forward Pass
+            pred = model(image)
+
+            # Compute Loss
+            loss = loss_criterion(pred, mask)
+
+            # Zero_grad/Backprop Loss/ Step Optimizer
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        # Save Model Checkpoint
+        checkpoint = {
+            "state_dict": model.state_dict(),
+            "optimizer": optimizer.state_dict(),
+            }
+
+        torch.save(checkpoint, f'C:\\Users\\Indy-Windows\\Documents\\RGB-D-Plant-Segmentation\\RGB_Segmentation\\old_school_models\\model_epoch{epoch}.ckpt')
+
+        val_loss = []
+        # Compute Validation Loss
+        for val_img, val_mask in tqdm(validation_dl):
+
+            print(f"Validation Image Size = {val_img.shape}")
+            # Send Training Data to GPU
+            val_img = val_img.to(device)
+            val_mask = val_mask.to(device)
+
+            # UNET Forward Pass
+            val_pred = model(val_img)
+
+            # Compute Loss
+            val_loss.append(loss_criterion(val_pred, val_mask))
+
+        print(f"Mean Validation Loss = {torch.mean(val_loss)} ")
+
